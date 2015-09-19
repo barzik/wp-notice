1f01664524f438d69005c60cdcd9521397cf7ed4
Ran Bar-Zik <ran@bar-zik.com>
2015-09-19 09:07:55 +0300

    testing the tracert for develop.wordpress

--- tests/bin/install-wp-tests.sh
+++ tests/bin/install-wp-tests.sh
@@ -78,8 +78,8 @@ install_test_suite() {
       else
         local TEST_BRANCH_NAME='branches/'$(sed 's/\([0-9]*\.[0-9]*\).*/\1/' <<< $WP_VERSION)
       fi
-    traceroute svn.wordpress.org
-    svn co --quiet https://develop.svn.wordpress.org/${TEST_BRANCH_NAME}/tests/phpunit/includes/ $WP_TESTS_DIR
+    
+    svn co --quiet https://develop.svn.wordpress.org/trunk/tests/phpunit/includes/ $WP_TESTS_DIR
   fi
 
   cd $WP_TESTS_DIR
