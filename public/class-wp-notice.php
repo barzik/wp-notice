57b85df42ad753c6a924fe4c7c0a4f2c886dbfad
Ran Bar-Zik <ran@bar-zik.com>
2015-09-22 17:05:55 +0300

    finishing wp-notice link linting and uninstall linting

--- public/class-wp-notice.php
+++ public/class-wp-notice.php
@@ -6,7 +6,7 @@
  * @author    Ran Bar-Zik <ran@bar-zik.com>
  * @license   GPL-2.0+
  * @link      http://internet-israel.com
- * @copyright 2014 Ran Bar-Zik
+ * @copyright 2015 Ran Bar-Zik
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
